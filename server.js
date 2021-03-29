f845fbdb7a24af8f573a870b30b133f920bc4fe9
Zanardelli Julien <jzanardelli@editpress.lu>
2021-03-29 17:00:23 +0200

    Endpoint : Get random quote

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+//create a route for quote route
+const quoteApiRouter = express.Router();
+app.use('/api/quote', quoteApiRouter );
+
+
+//endpoint Get Random Quote
+quoteApiRouter.get( '/', (req, res, next) => {
+
+    const elem = getRandomElement(quotes);
+    res.status(200).send(elem)
+});
+
+app.listen(PORT);
