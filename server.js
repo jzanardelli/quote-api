4b67b687dc588b22585aaaed14dab8a48e92a05d
Zanardelli Julien <jzanardelli@editpress.lu>
2021-03-29 17:26:17 +0200

    Add all quotes endpoint

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ app.use(express.static('public'));
 
 //create a route for quote route
 const quoteApiRouter = express.Router();
-app.use('/api/quote', quoteApiRouter );
+app.use('/api', quoteApiRouter );
 
 
 //endpoint Get Random Quote
-quoteApiRouter.get( '/', (req, res, next) => {
+quoteApiRouter.get( '/quote/random', (req, res, next) => {
 
     const elem = getRandomElement(quotes);
     res.status(200).send(elem)
 });
+
+//endpoint Get all quotes
+quoteApiRouter.get( '/quotes', (req, res, next) => {
+    const person = req.query.person;
+
+    if (person) {
+       // find index of person
+       const index =  quotes.findIndex( quote => quote.person === person );
+       res.status(200).send( quotes[index]);
+    }
+    else res.status(200).send(quotes);
+});
+
 
 app.listen(PORT);
